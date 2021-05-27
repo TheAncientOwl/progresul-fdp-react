a0f99d6c6543696fa0ff2bfecfc267b7d8da1643
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-05-27 18:28:32 +0300

    moved 'Aspecte' into its own file.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import Navbar from './components/navbar';
-import {
-  ContentContainer,
-  SectionTitle,
-  Paragraph,
-  SectionList,
-  SectionListItem,
-  SectionListItemDesc,
-  ParagraphIndent,
-  Divider,
-  SectionSubTitle,
-} from './components/content/ContentElements';
+import { ContentContainer } from './components/content/ContentElements';
 import ContentSection from './components/content';
 import theme from './theme';
 import TipuriDeCapital from './components/sections/capitalul-tehnic-si-progresul-fdp/TipuriDeCapital';
+import Aspecte from './components/sections/capitalul-tehnic-si-progresul-fdp/Aspecte';
 
 export default function App() {
   const items = [
@@ -30,84 +21,7 @@ export default function App() {
           <Navbar title='Capitalul tehnic si progresul factorilor de productie' items={items} />
           <ContentContainer>
             <TipuriDeCapital id='tipuri-de-capital' />
-            <ContentSection id='aspecte'>
-              <SectionTitle>Aspectul calitativ si cantitativ al factorilor de producție</SectionTitle>
-              <Paragraph>
-                <ParagraphIndent />
-                PROGRESUL FACTORILOR DE PRODUCȚIE - reprezintă un ansamblu de realizări extreme de diverse ce poate fi
-                pus în evidență prin surprinderea celor mai importante tendințe de schimbare, cantitative și calitative,
-                care s-au manifestat pe termen foarte lung în sfera economicului.
-              </Paragraph>
-              <Divider />
-              <Paragraph>
-                <ParagraphIndent />
-                Așadar, se impun două aspecte ale progresului factorilor de producție, care pot fi urmărite ca atare dar
-                care sunt, în același timp, interdependente:
-                <SectionList>
-                  {['- unul cantitativ;', '- unul calitativ.'].map((item, index) => (
-                    <SectionListItem key={index}>{item}</SectionListItem>
-                  ))}
-                </SectionList>
-              </Paragraph>
-              <Divider />
-              <SectionSubTitle>Aspectul cantitativ</SectionSubTitle>
-              <Paragraph>
-                <ParagraphIndent />
-                Aspectul cantitativ este relevat de două tendințe:
-                <SectionList>
-                  {[
-                    {
-                      value: 'a) Tendința generală de creștere a factorilor de producție:',
-                      desc: 'un număr mai mare de factori de producție înseamnă și o mai mare diversificare a lor;',
-                    },
-                    {
-                      value: 'b) Marirea volumului de factori de producție utilizați:',
-                      desc: 'acest lucru reprezintă un proces deoarece, în acest fel, s-au putut asigura cantități tot mai mari de factori de producție și, prin bunurile diverse create, au putut fi satisfăcute tot mai multe trebuințe individuale și sociale.',
-                    },
-                  ].map((item, index) => (
-                    <React.Fragment key={index}>
-                      <SectionListItem>{item.value}</SectionListItem>
-                      <SectionListItemDesc>{item.desc}</SectionListItemDesc>
-                    </React.Fragment>
-                  ))}
-                </SectionList>
-                <br />
-                <ParagraphIndent />
-                Un aspect cantitativ al progresului factorilor de producție este utilizarea factorilor de producție pe
-                principii ecologice
-              </Paragraph>
-              <Divider />
-              <SectionSubTitle>Aspectul calitativ</SectionSubTitle>
-              <Paragraph>
-                <ParagraphIndent />
-                Aspectul calitativ este pus în evidență prin tendințele urmatoare:
-                <SectionList>
-                  {[
-                    'a) Creșterea raționalității utilizarii factorilor de producție, a eficienței utilizării lor, având ca efect diminuarea consumului de factori de producție pe unitate de produs (bun) sau de trebuință satisfăcută;',
-                    'b) Extinderea utilizării factorilor de producție la noi activități prin combinarea lor în variante și cu rezultate tot mai diferite;',
-                    <>
-                      c) Utilizarea factorilor de producție pe principii ecologice urmărindu-se:
-                      <SectionList>
-                        {[
-                          '- Evitarea degradării mediului ambient;',
-                          '- Menținerea parametrilor de mediu în limite normale pentru existența umană',
-                        ].map((item, index) => (
-                          <SectionListItem key={index}>{item}</SectionListItem>
-                        ))}
-                      </SectionList>
-                    </>,
-                  ].map((item, index) => (
-                    <SectionListItem key={index}>{item}</SectionListItem>
-                  ))}
-                </SectionList>
-              </Paragraph>
-              <Divider />
-              <Paragraph>
-                <ParagraphIndent />
-                Procesul de recuperare treptată a valorii capitalului se numește amortizare.
-              </Paragraph>
-              <Divider />
-            </ContentSection>
+            <Aspecte id='aspecte' />
             <ContentSection id='dezvoltarea-economica'></ContentSection>
           </ContentContainer>
         </>
