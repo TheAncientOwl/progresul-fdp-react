d6d972fa515a13ab54ce83850afd5ca092afd795
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-05-27 13:31:27 +0300

    moved theme in ist own file.

--- src/App.js
+++ src/App.js
@@ -16,24 +16,7 @@ import {
 import ContentSection from './components/content';
 import ComponenteleCapitaluluiGr from './images/componentele_capitalului.png';
 import Graphic from './components/graphic';
-
-const theme = {
-  primary: '#50E627',
-  secondary: '#36991A',
-  nav: {
-    background: '#131313',
-    title: {
-      gradient: {
-        col1: '#062601',
-        col2: '#318c18',
-      },
-    },
-  },
-  text: {
-    primary: '#959595',
-    secondary: '#3D3D3D',
-  },
-};
+import theme from './theme';
 
 export default function App() {
   const items = [
---
+++ src/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+  primary: '#50E627',
+  secondary: '#36991A',
+  nav: {
+    background: '#131313',
+    title: {
+      gradient: {
+        col1: '#062601',
+        col2: '#318c18',
+      },
+    },
+  },
+  text: {
+    primary: '#959595',
+    secondary: '#3D3D3D',
+  },
+};
+
+Object.freeze(theme);
+export default theme;
