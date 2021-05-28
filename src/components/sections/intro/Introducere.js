daf391b8b5c171ad1d878e1851893741d6c8d4cc
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-05-29 01:56:31 +0300

    added 'Introducere'.

---
+++ src/components/sections/intro/Introducere.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ContentSection from '../../content';
+import {
+  Divider,
+  Paragraph,
+  ParagraphIndent,
+  SectionList,
+  SectionListItem,
+  SectionTitle,
+} from '../../content/ContentElements';
+
+export default function Introducere({ id }) {
+  return (
+    <ContentSection id={id}>
+      <SectionTitle>Introducere</SectionTitle>
+      <Paragraph style={{ width: '55%', marginRight: 0, fontStyle: 'italic' }}>
+        <ParagraphIndent />
+        &quot;Așa cum îl folosesc economiștii, capitalul înseamnă mijloace produse pentru producție sau bunuri care pot
+        fi folosite pentru a produce bunuri viitoare. Mașinile sunt capital ca și clădirile industriale si comerciale.
+        Dar, la fel sunt cunoștintele și măiestria profesională pe care oamenii le acumulează prin educație, calificare
+        sau experiență și care le permit să furnizeze servicii productive valoroase pentru ceilalți.&quot;{' '}
+        <sup>[1]</sup>
+      </Paragraph>
+      <Divider />
+      <Paragraph>
+        <ParagraphIndent />
+        Pentru dezvoltarea unei economii progresul tehnic este, în același timp, premisă și rezultat.
+      </Paragraph>
+      <Divider />
+      <Paragraph>
+        <ParagraphIndent />
+        FACTORII DE PRODUCȚIE:
+        <SectionList>
+          {[
+            '- reprezintă ansamblul elementelor care participă la producerea bunurilor si serviciilor;',
+            '- sunt organic legați de resurse, de aceea pot fi definiți și prin raportarea la resurse;',
+            '- constituie partea bunurilor atrase, alocate și consumate în procesul de producere a bunurilor economice.',
+          ].map((item, index) => (
+            <SectionListItem key={index}>{item}</SectionListItem>
+          ))}
+        </SectionList>
+      </Paragraph>
+      <Divider />
+      <Paragraph>
+        <ParagraphIndent />
+        Prin consumul factorilor de producție:
+        <SectionList>
+          {[
+            '- se creează noi bunuri și servicii;',
+            '- se dezvoltă cunoștințele tehnico-științifice;',
+            '- se descoperă noi resurse naturale;',
+            '- se creează condiții pentru îmbunătățirea calitativă și structurală a resurselor.',
+          ].map((item, index) => (
+            <SectionListItem key={index}>{item}</SectionListItem>
+          ))}
+        </SectionList>
+      </Paragraph>
+      <Divider />
+      <Paragraph style={{ fontStyle: 'italic' }}>
+        <ParagraphIndent />
+        <sup>1</sup>(Paul Heyne - ”Modelul economic de gândire. Mersul economiei de piață”, E.D.P., Bucuresti, 1990)
+      </Paragraph>
+      <Divider />
+    </ContentSection>
+  );
+}
+
+Introducere.propTypes = {
+  id: PropTypes.string.isRequired,
+};
