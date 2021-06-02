a06e180a02c624334a3829399e92f4953c1ada3e
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-06-03 02:17:00 +0300

    Updates

--- src/Breakpoints.js
+++
@@ -1,9 +0,0 @@
-const Breakpoints = {
-  desktop: '1024px',
-  tablet: '768px',
-  phone: '680px',
-  miniPhone: '300px',
-};
-
-Object.freeze(Breakpoints);
-export default Breakpoints;
--- src/components/copyright/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { CopyrightContainer } from './CopyrightElements';
-
-export default function Copyright() {
-  return <CopyrightContainer>~ Copyright © Delegeanu Alexandru 2021 ~</CopyrightContainer>;
-}
